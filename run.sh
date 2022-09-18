adc02197497c1fe38d3dbe97f46cfbe4ec9cfe49
Amir Omidi <amir@aaomidi.com>
2022-09-18 21:00:53 +0000

    Fix tag

--- run.sh
+++ run.sh
@@ -2,12 +2,14 @@
 
 export PATH=$PATH:/usr/local/bin
 
+echo "Starting tailscaled"
 /app/tailscaled --state=/state/tailscaled.state --statedir=/state/ &
 
 # Wait for tailscale
 sleep 3
 
-tailscale up ${UP_FLAGS:-}
+echo "Starting tailscale"
+/app/tailscale up ${UP_FLAGS:-}
 
 
 # Do nothing
